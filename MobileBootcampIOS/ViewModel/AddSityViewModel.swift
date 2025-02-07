669780140581b3e213d77062c4760f9f2f085f12
Kirill Sheremetev <85827294+Rote-cloud@users.noreply.github.com>
2025-02-07 12:04:49 +0600

    feature: add ViewModels

---
+++ MobileBootcampIOS/ViewModel/AddSityViewModel.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+
+final class AddSityViewModel: ObservableObject {
+
+    private let router: AddSityPageRouter
+
+    init(router: AddSityPageRouter) {
+        self.router = router
+    }
+
+    func navigateToMainPage() {
+        self.router.routeToMainPage()
+    }
+}
+
+// MARK: - HomePageViewModel mock for preview
+
+extension AddSityViewModel {
+    static let mock: AddSityViewModel = .init(router: AddSityPageRouter.mock)
+}
--- MobileBootcampIOS/ViewModel/MainViewModel.swift
+++ MobileBootcampIOS/ViewModel/MainViewModel.swift
@@ -1,1 +1,58 @@
+import Foundation
+import SwiftUI
 
+final class MainViewModel: ObservableObject {
+
+    enum MainViewModelState: Equatable {
+        case loaded
+        case loading
+        case error
+    }
+
+    private let router: MainPageRouter
+    private let locationManager: LocationManager
+    private var weatherManager: WeatherManagerProtocol
+
+    @Published var weather: WeatherEntity = .stub
+    @Published var state: MainViewModelState = .loading
+
+    init(router: MainPageRouter) {
+        self.router = router
+        self.locationManager = LocationManager()
+        self.weatherManager = WeatherManager()
+        self.updateLocation()
+    }
+
+    func navigateToAddSityPage() {
+        self.router.routeToAddSityPage()
+    }
+
+    func updateLocation() {
+        locationManager.requestLocation { [weak self] city in
+            guard let self = self else { return }
+            self.weatherManager.city = city
+            self.getWeatherForCity()
+        }
+    }
+
+    func getWeatherForCity() {
+        self.state = .loading
+        weatherManager.fetchWeather { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let weather):
+                    self?.weather = weather
+                    self?.state = .loaded
+                case .failure(_):
+                    self?.state = .error
+                }
+            }
+        }
+    }
+}
+
+// MARK: - HomePageViewModel mock for preview
+
+extension MainViewModel {
+    static let mock: MainViewModel = .init(router: MainPageRouter.mock)
+}
